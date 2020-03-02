9d3ffe0f42803df7df8039801dbc911374ea618b
Leitao <uncodead@gmail.com>
2020-03-02 16:18:42 -0300

    envs

--- reprocess/settings.py
+++ reprocess/settings.py
@@ -1,21 +1,21 @@
 import os
 
 SCHEMA = {
-    'uri': os.environ.get('SCHEMA_URI', 'http://localhost:9092/api/v1/'),
+    'uri': os.environ.get('SCHEMA_URI', 'http://schema:9092/api/v1/'),
 }
 
 PROCESS_MEMORY = {
-    'api_url': os.environ.get('PROCESS_MEMORY_URI', 'http://localhost:9091/'),
+    'api_url': os.environ.get('PROCESS_MEMORY_URI', 'http://process_memory:9091/'),
 }
 
 EVENT_MANAGER = {
-    'api_url': os.environ.get('EVENT_MANAGER_URI', 'http://localhost:8081/'),
+    'api_url': os.environ.get('EVENT_MANAGER_URI', 'http://event_manager:8081/'),
 }
 
 DOMAIN_READER = {
-    'api_url': os.environ.get('DOMAIN_READER_URI', 'http://localhost:9093/reader/api/v1/'),
+    'api_url': os.environ.get('DOMAIN_READER_URI', 'http://domain_reader:9093/reader/api/v1/'),
 }
 
 REPROCESS_SETTINGS = {
-    'host': 'localhost'
+    'host': os.environ.get('RABBIT_MQ', 'rabbitmq'),
 }
