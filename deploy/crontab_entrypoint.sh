f80a340cd8758d5fd69eef8f240b0925aa98beb3
Leitao <uncodead@gmail.com>
2020-03-03 15:34:11 -0300

    crontab

--- deploy/crontab_entrypoint.sh
+++ deploy/crontab_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Setup a cron schedule
-echo "* * * * * /reprocess.sh >> /var/log/reprocess_cron.log 2>&1
+echo "* * * * * /reprocess.sh >> /var/cron/reprocess/log/reprocess_cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
--- deploy/reprocess.sh
+++ deploy/reprocess.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "System path is $PATH at $timestamp"
+
+python reprocess/reprocess_executor.py
