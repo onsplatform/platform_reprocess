776621470711189bf614554176e3688ee906686f
Leitao <uncodead@gmail.com>
2020-03-03 18:22:57 -0300

    compose

--- deploy/reprocess.sh
+++ deploy/reprocess.sh
@@ -5,8 +5,13 @@ echo "System path is $PATH at $timestamp"
 python -V
 python3 -V
 
+export SCHEMA_URI=http://schema/api/v1/
+export EVENT_MANAGER_URI=http://event_manager:8081/
+export RABBIT_MQ=rabbitmq
+
 env
 
 cd /var/cron/reprocess
 
 /usr/local/bin/pipenv run python reprocess/reprocess_executor.py
+
