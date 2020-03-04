7649ad72ac4b0503f2a739a287f33d90d08e3ce5
Leitao <uncodead@gmail.com>
2020-03-04 10:15:29 -0300

    new crontab

--- deploy/crontab_entrypoint.sh
+++ deploy/crontab_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Setup a cron schedule
-echo "* * * * * /var/cron/reprocess/deploy/reprocess.sh >> /var/cron/reprocess/log/reprocess_cron.log 2>&1
+echo "* * * * * /reprocess.sh >> /var/cron/reprocess/log/reprocess_cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
