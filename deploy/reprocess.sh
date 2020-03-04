2c68e06b1d27b38dbc300bfa0a9730fa49122dfd
Leitao <uncodead@gmail.com>
2020-03-04 10:11:39 -0300

    new crontab

--- deploy/reprocess.sh
+++ deploy/reprocess.sh
@@ -2,16 +2,10 @@
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "System path is $PATH at $timestamp"
 
-python -V
-python3 -V
-
 export SCHEMA_URI=http://schema/api/v1/
 export EVENT_MANAGER_URI=http://event_manager:8081/
 export RABBIT_MQ=rabbitmq
 
-env
-
 cd /var/cron/reprocess
 
 /usr/local/bin/pipenv run python reprocess/reprocess_executor.py
-
